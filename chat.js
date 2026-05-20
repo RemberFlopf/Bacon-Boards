45951b29d1a27640e39a61e628e747b57c02c2e0
RemberFlopf <charliedtbirbeck@gmail.com>
2026-05-20 22:40:25 +0930

    Minor optimisations and notifications
    Only loads last 100 messages on page open and plays a cool beep noise when someone sends a message

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getDatabase, ref, push, set, get, remove, onValue } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+import { getDatabase, ref, push, set, get, remove, onValue, onChildAdded, limitToLast, query } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 const firebaseConfig = {
 
@@ -19,9 +19,12 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getDatabase(app, "https://chat-app-test-bb975-default-rtdb.asia-southeast1.firebasedatabase.app/");
+const db = getDatabase(app, "wss://chat-app-test-bb975-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
 const baconChat = ref(db, 'baconChat')
+
+let viewing = true;
+let notif = new Audio('./notif.wav');
 
 document.getElementById('baconChatForm').addEventListener('submit', (e) => {e.preventDefault(); addToBaconChat()})
 
@@ -35,7 +38,7 @@ document.addEventListener('keyup', (event) => {
 
 })
 
-onValue(baconChat, (message) => {
+onChildAdded(query(baconChat, limitToLast(100)), (message) => {
 
     if (message.exists()){
 
@@ -57,36 +60,49 @@ async function updateBaconChat(currentData){
 
     document.querySelectorAll('.bacon-Chat').forEach(p => p.remove())
 
-    currentData.forEach(object => {
+    let baconChat = document.getElementById('baconChat');
+    let p = document.createElement('div')
 
-        let baconChat = document.getElementById('baconChat');
-        let p = document.createElement('div')
+    if (currentData.val().type === 'image'){
 
-        if (object.val().type === 'image'){
+        let img = new Image();
+        
+        fetch(currentData.val().chat)
+        .then(res => res.blob())
+        .then(blob => {
 
-            let img = new Image();
-            
-            img.src = object.val().chat;
+            const blobUrl = URL.createObjectURL(blob);
+            img.src = blobUrl;
 
-
-            p.innerHTML = `<b>${object.val().name}</b> : `;
-
-            p.appendChild(img);
-
-        }
-
-        else{
-
-            p.innerHTML = `<b>${object.val().name}</b> : ${object.val().chat}`;
-
-        }
+        });
 
 
-        //p.classList.add('bacon-ChatA')
-        baconChat.appendChild(p)
-        baconChat.scrollTop = baconChat.scrollHeight;
 
-    })
+        img.loading = 'lazy';
+
+        p.innerHTML = `<b>${currentData.val().name}</b> : `;
+
+        p.appendChild(img);
+
+    }
+
+    else{
+
+        p.innerHTML = `<b>${currentData.val().name}</b> : ${currentData.val().chat}`;
+
+    }
+
+    //p.classList.add('bacon-ChatA')
+    baconChat.appendChild(p)
+    baconChat.scrollTop = baconChat.scrollHeight;
+
+    if (currentData.val().name !== document.querySelector('#username').value.trim() && !viewing){
+
+        console.log('attemping to play sound')
+
+        notif.play();
+
+    }
 
 }
 
@@ -123,6 +139,8 @@ async function addToBaconChat(){
     let username = document.getElementById('username');
     let text = document.getElementById('baconChatInput');
 
+    let user = username.value.trim();
+
     let chat = text.value.trim();
 
     if (!chat || chat.length > 60){
@@ -132,7 +150,7 @@ async function addToBaconChat(){
 
     }
 
-    if (username.value.trim().length > 25){
+    if (user.length > 25){
 
         username.style.borderColor = 'red';
         return;
@@ -142,9 +160,10 @@ async function addToBaconChat(){
     username.style.borderColor = '#d69286';
     text.style.borderColor = '#d69286';
 
-    set(push(baconChat), {chat : chat, name : document.getElementById('username').value.trim() || "Anonymous", type : 'text'})
+    set(push(baconChat), {chat : chat, name : user || "Anonymous", type : 'text'})
 
-    document.getElementById('baconChatForm').reset()
+    document.getElementById('baconChatInput').value = '';
+    //document.getElementById('baconChatForm').reset()
 
 }
 
@@ -166,4 +185,15 @@ async function clearBaconChat(currentData){
 
 }
 
+function notfiy(){
+
+    document.addEventListener('visibilitychange', (event) => {
+
+        viewing = !document.hidden;
+
+    })
+
+}
+
+notfiy();
 encodeImageFile();
